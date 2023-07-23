628c58a7981c3d48ae23b08b9c4145431141ed58
Jean-Luc <kedric@me.com>
2023-07-23 11:23:48 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/gokutils/multiserver
+
+go 1.20
+
+require (
+	github.com/improbable-eng/grpc-web v0.15.0
+	github.com/soheilhy/cmux v0.1.5
+	golang.org/x/sync v0.3.0
+	google.golang.org/grpc v1.56.2
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
+	github.com/gin-gonic/gin v1.9.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/rs/cors v1.7.0 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	nhooyr.io/websocket v1.8.6 // indirect
+)
---
+++ multisever.go
@@ -0,0 +1,123 @@
+package multiserver
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"github.com/soheilhy/cmux"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+)
+
+type ServeHTTP interface {
+	ServeHTTP(res http.ResponseWriter, req *http.Request) error
+}
+
+type MultiServer struct {
+	GrpcWeb    *grpcweb.WrappedGrpcServer
+	GrpcServer *grpc.Server
+	HttpServer ServeHTTP
+}
+
+func (impl *MultiServer) IsGrpcWeb(req *http.Request) bool {
+	return impl.GrpcWeb.IsGrpcWebRequest(req) || impl.GrpcWeb.IsGrpcWebSocketRequest(req) || impl.GrpcWeb.IsAcceptableGrpcCorsRequest(req)
+}
+
+func (impl *MultiServer) IsPProof(req *http.Request) bool {
+	return strings.HasPrefix(req.URL.Path, "/debug/pprof")
+}
+
+func (impl *MultiServer) DefereHTTPRecover() {
+	if err := recover(); err != nil {
+		log.Printf("[server] Recover %v", err)
+	}
+}
+
+func (impl *MultiServer) DefereHTTPTrace(req *http.Request, start time.Time) {
+	log.Printf("%s Duration: %s ", req.URL.Path, time.Since(start))
+}
+
+func (impl *MultiServer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	defer impl.DefereHTTPTrace(req, time.Now())
+	defer impl.DefereHTTPRecover()
+	if impl.IsGrpcWeb(req) {
+		req.URL.Path = strings.TrimPrefix(req.URL.Path, "/grpc-web")
+		impl.GrpcWeb.ServeHTTP(res, req)
+		return
+	}
+	if impl.IsPProof(req) {
+		http.DefaultServeMux.ServeHTTP(res, req)
+		return
+	}
+	impl.HttpServer.ServeHTTP(res, req)
+}
+
+func (impl *MultiServer) ServeListener(ctx context.Context, l net.Listener) error {
+	m := cmux.New(l)
+	grpcListener := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
+	httpListener := m.Match(cmux.Any())
+	warperHttp := &http.Server{
+		Handler: impl,
+	}
+	g := errgroup.Group{}
+	g.Go(func() error {
+		return impl.GrpcServer.Serve(grpcListener)
+	})
+
+	g.Go(func() error {
+		return warperHttp.Serve(httpListener)
+	})
+
+	g.Go(func() error {
+		return m.Serve()
+	})
+	running := true
+	g.Go(func() error {
+		<-ctx.Done()
+		running = false
+		m.Close()
+		l.Close()
+		return nil
+	})
+	addr := l.Addr().String()
+	log.Printf("[Server] running open your browser on %s", addr)
+	err := g.Wait()
+	if err != nil && running {
+		return err
+	}
+	return nil
+}
+
+func (impl *MultiServer) ListenAndServe(ctx context.Context, address string) error {
+	l, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+	return impl.ServeListener(ctx, l)
+}
+
+func warpServer(server *grpc.Server) *grpcweb.WrappedGrpcServer {
+	return grpcweb.WrapServer(
+		server,
+		// Enable CORS
+		grpcweb.WithOriginFunc(func(origin string) bool { return true }),
+		grpcweb.WithWebsockets(true),
+		grpcweb.WithWebsocketOriginFunc(func(req *http.Request) bool {
+			return true
+		}),
+		grpcweb.WithAllowNonRootResource(true),
+	)
+}
+
+func NewServer(ctx context.Context, grpcServer *grpc.Server, httpServer ServeHTTP) (*MultiServer, error) {
+	return &MultiServer{
+		GrpcServer: grpcServer,
+		GrpcWeb:    warpServer(grpcServer),
+		HttpServer: httpServer,
+	}, nil
+}
